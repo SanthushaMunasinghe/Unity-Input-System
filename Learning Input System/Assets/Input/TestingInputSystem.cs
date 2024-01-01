cc86ee086c4b3acff4eb12ca3bc530bb6a3a176f
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2024-01-01 21:33:16 +0530

    Action Maps

--- Learning Input System/Assets/Input/TestingInputSystem.cs
+++ Learning Input System/Assets/Input/TestingInputSystem.cs
@@ -5,12 +5,38 @@ using UnityEngine.InputSystem;
 
 public class TestingInputSystem : MonoBehaviour
 {
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void ChangeToUI()
+    {
+        playerInput.SwitchCurrentActionMap("UI");
+    }
+    
+    public void ChangeToPlayer()
+    {
+        playerInput.SwitchCurrentActionMap("Player");
+    }
+
     public void Jump(InputAction.CallbackContext ctx)
     {
-        Debug.Log(ctx);
-        //if (ctx.performed)
-        //{
-        //    Debug.Log("Jump " + ctx.phase);
-        //}
+        //Debug.Log(ctx);
+        if (ctx.performed)
+        {
+            Debug.Log("Jump " + ctx.phase);
+        }
+    }
+    
+    public void Submit(InputAction.CallbackContext ctx)
+    {
+        //Debug.Log(ctx);
+        if (ctx.performed)
+        {
+            Debug.Log("Submit " + ctx.phase);
+        }
     }
 }
