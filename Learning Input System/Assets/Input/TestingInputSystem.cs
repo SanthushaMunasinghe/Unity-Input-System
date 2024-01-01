6fc0ab57cc99eba4de4a76df480a65692040434b
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2024-01-01 17:41:18 +0530

    Basic Input System Using Unity Events

---
+++ Learning Input System/Assets/Input/TestingInputSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestingInputSystem : MonoBehaviour
+{
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            Debug.Log("Jump " + ctx.phase);
+        }
+    }
+}
