e320e0e75b2e8b5a83459b2752ae299e705b6b54
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2024-01-01 19:20:51 +0530

    Movement

--- Learning Input System/Assets/Input/TestingInputSystem.cs
+++ Learning Input System/Assets/Input/TestingInputSystem.cs
@@ -5,18 +5,33 @@ using UnityEngine.InputSystem;
 
 public class TestingInputSystem : MonoBehaviour
 {
-    private PlayerInput playerInput;
+    private Rigidbody rb;
+    private PlayerInputActions playerInputActions;
 
     private void Awake()
     {
-        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
 
-        playerInput.onActionTriggered += PlayerInput_onActionTriggered;
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+        //playerInputActions.Player.Movement.performed += Movement_performed;
     }
 
-    private void PlayerInput_onActionTriggered(InputAction.CallbackContext obj)
+    private void Movement_performed(InputAction.CallbackContext obj)
     {
-        Debug.Log(obj);
+        Vector2 inputVector = obj.ReadValue<Vector2>();
+        float speed = 5f;
+
+        rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        float speed = 20.0f;
+
+        rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
     }
 
     public void Jump(InputAction.CallbackContext ctx)
@@ -24,6 +39,7 @@ public class TestingInputSystem : MonoBehaviour
         if (ctx.performed)
         {
             Debug.Log("Jump " + ctx.phase);
+            rb.AddForce(Vector3.up * 3.0f, ForceMode.Impulse);
         }
     }
 }
